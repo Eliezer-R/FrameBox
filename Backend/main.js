42855fcc056509eebef8dd7b0529c97c8fafac5b
Eliezer .R <ronaldalmanzarvasques@gmail.com>
2025-07-24 22:43:33 -0400

    Update main.js

--- Backend/main.js
+++ Backend/main.js
@@ -18,7 +18,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'] 
 }))
 
